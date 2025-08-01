03eb6bec225b092fd434ae0404bf7b9ecb9ef804
SOUNDARYAS45 <sonuaishu08@gmail.com>
2025-08-01 06:49:30 +0000

    First Co mmit

--- docker1/app1.py
+++ docker1/app1.py
@@ -8,4 +8,4 @@ def read_root():
     return {"message": "Hello from Dockerized FastAPI App!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
